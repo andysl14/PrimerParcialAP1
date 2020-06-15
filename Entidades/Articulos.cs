a187256f679cc9405655152e0fada42323ce0ddb
Andy Stward Lanfranco Ayala <andy_lanfranco@ucne.edu.do>
2020-06-15 18:29:12 -0400

    Se creo la clase Entidades

---
+++ Entidades/Articulos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace PrimerParcial.Entidades
+{
+    public class Articulos
+    {
+        [Key]
+        public int ArticuloId { get; set; }
+        public string Descripcion { get; set; }
+        public int Existencia { get; set; }
+        public float Costo { get; set; }
+        public float Valor_Inventario { get; set; }
+    }
+}
