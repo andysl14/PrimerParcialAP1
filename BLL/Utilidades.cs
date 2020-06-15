5a2aef404bff689c200a60bc923ac07becee934f
Andy Stward Lanfranco Ayala <andy_lanfranco@ucne.edu.do>
2020-06-15 19:40:52 -0400

    Creando UtilidadesBLL

---
+++ BLL/Utilidades.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrimerParcial.BLL
+{
+    public class Utilidades
+    {
+        public static int Toint(string valor)
+        {
+            int retorno = 0;
+
+            int.TryParse(valor, out retorno);
+
+            return retorno;
+        }
+    }
+}
---
+++ Migrations/20200615233700_Initial_Migration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PrimerParcial.Migrations
+{
+    public partial class Initial_Migration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Articulos",
+                columns: table => new
+                {
+                    ArticuloId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Descripcion = table.Column<string>(nullable: true),
+                    Existencia = table.Column<int>(nullable: false),
+                    Costo = table.Column<float>(nullable: false),
+                    Valor_Inventario = table.Column<float>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Articulos", x => x.ArticuloId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Articulos");
+        }
+    }
+}
