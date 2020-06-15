c3d7748df1ea825d470734859abe7f1129936364
Andy Stward Lanfranco Ayala <andy_lanfranco@ucne.edu.do>
2020-06-15 19:55:44 -0400

    Programando el XAML.CS para hacer funcionar los botones

---
+++ UI/Registros/rArticulos.xaml.cs
@@ -0,0 +1,29 @@
+﻿using PrimerParcial.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PrimerParcial.UI.Registros
+{
+    /// <summary>
+    /// Interaction logic for rArticulos.xaml
+    /// </summary>
+    public partial class rArticulos : Window
+    {
+        private Articulos Articulo = new Articulos();
+        public rArticulos()
+        {
+            InitializeComponent();
+
+            this.DataContext = Articulo;
+        }
+    }
+}
