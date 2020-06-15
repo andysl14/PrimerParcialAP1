55535032ab89041c5dd1b6ea9550eb728b5ad974
Andy Stward Lanfranco Ayala <andy_lanfranco@ucne.edu.do>
2020-06-15 17:46:15 -0400

    Creando clase Contexto y subiendo un cambio en la carpeta resources

---
+++ DAL/Contexto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrimerParcial.DAL
+{
+    public class Contexto : DbContext
+    {
+
+    }
+}
